a754a24c0576552ac4efb5aaab39b472bef1f28f
Jefferson Alquinga <jefferson.alquinga1998@gmail.com>
2024-05-23 00:37:38 -0500

    Final de header

---
+++ views/templates/header.php
@@ -0,0 +1,43 @@
+<header class="header">
+    <div class="header__contenedor">
+    <nav class="header__navegacion">
+        <a href="/registro" class="header__enlace">Registro</a>
+        <a href="/login"  class="header__enlace">Iniciar Sesión</a>
+    </nav>
+
+    <div class="header__contenido">
+        <a href="../login">
+            <h1 class="header__logo">
+                &#60;MILENYUM-DOG />
+                </h1>
+
+                </a>
+
+                <p class="header__texto">Septiembre 4-5 - 2024</p>
+                <p class="header__texto header__texto--modalidad">En Línea - Presencial</p>
+                <a href="/registro" class="header__boton">Comprar Pase</a>
+            </h1>
+        </a>
+    </div>
+
+    </div>
+</header>
+
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="../login">
+        <h2 class="barra__logo">   &#60;MILENYUM-DOG />
+            
+        </h2>
+        </a>
+        <nav class="navegacion">
+            <a href="/chatdog" class="navegacion__enlace">Chat Dog</a>
+            <a href="/milenyum-dog" class="navegacion__enlace">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+            <a href="/workshops-conferencias" class="navegacion__enlace">Workshops / Conferencias</a>
+            <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+        </nav>
+    </div>
+
+
+</div>
